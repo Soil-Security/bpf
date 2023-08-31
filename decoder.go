e175a794f6bdb93d74c08091f3415cc3692a67aa
Daniel Pacak <1322923+danielpacak@users.noreply.github.com>
2023-08-31 19:40:47 +0200

    feat: decode u64

--- decoder.go
+++ decoder.go
@@ -11,24 +11,42 @@ type Decoder struct {
 	ByteOrder binary.ByteOrder
 }
 
-func (e *Decoder) Byte(buf []byte, off int) (byte, int, error) {
+func (d *Decoder) Byte(buf []byte, off int) (byte, int, error) {
 	if off+1 > len(buf) {
 		return 0, off, errors.New("overflow unpacking byte")
 	}
 	return buf[off], off + 1, nil
 }
 
-func (e *Decoder) Uint32(buf []byte, off int) (uint32, int, error) {
+func (d *Decoder) Uint32(buf []byte, off int) (uint32, int, error) {
 	if off+4 > len(buf) {
 		return 0, off, errors.New("overflow unpacking uint32")
 	}
-	u := e.ByteOrder.Uint32(buf[off : off+4])
+	u := d.ByteOrder.Uint32(buf[off : off+4])
 	return u, off + 4, nil
 }
 
-func (e *Decoder) Uint32AsInt(buf []byte, off int) (int, int, error) {
-	i, o, err := e.Uint32(buf, off)
+func (d *Decoder) Uint32AsInt(buf []byte, off int) (int, int, error) {
+	i, o, err := d.Uint32(buf, off)
 	return int(i), o, err
+}
+
+func (d *Decoder) Uint64(buf []byte, off int) (uint64, int, error) {
+	if off+8 > len(buf) {
+		return 0, off, errors.New("overflow unpacking uint64")
+	}
+	u := d.ByteOrder.Uint64(buf[off : off+8])
+	return u, off + 8, nil
+}
+
+func (d *Decoder) Uint64AsInt(buf []byte, off int) (int, int, error) {
+	i, o, err := d.Uint64(buf, off)
+	return int(i), o, err
+}
+
+func (d *Decoder) Int64(buf []byte, off int) (int64, int, error) {
+	i, o, err := d.Uint64(buf, off)
+	return int64(i), o, err
 }
 
 func (e *Decoder) Str(buf []byte, off, sz int) (string, int, error) {
